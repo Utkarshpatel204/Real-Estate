2ba3dcf3af02a5bb25f44c0f6118073cdbbb4850
Utkarsh <utkarshpatel204@gmail.com>
2023-11-11 13:18:13 +0530

    complete create page listing UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -5,6 +5,7 @@ import { app } from "../firebase.js";
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
 import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserSuccess, signOutUserFailure, signOutUserStart } from "../redux/user/userSlice.js";
 import { useDispatch } from "react-redux";
+import {Link} from 'react-router-dom';
 
 function Profile() {
   const { currentUser, loading, error } = useSelector((state) => state.user);
@@ -167,6 +168,9 @@ function Profile() {
         <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
           {loading ? 'Loading...' : 'Update'}
         </button>
+        <Link className="bg-green-700 text-white uppercase p-3 rounded-lg text-center hover:opacity-95" to="/create-listing">
+          Create Listing
+        </Link>
       </form>
 
       <div className="flex justify-between mt-5">
