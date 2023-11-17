9cf6592afc7b3b9a3c8d3686d674cd32c25d4991
Utkarsh <utkarshpatel204@gmail.com>
2023-11-17 11:58:14 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.router.js';
-import cors from 'cors'
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -15,10 +15,11 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 });
 
+const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000!");
@@ -27,6 +28,11 @@ app.listen(3000,()=>{
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
 app.use("/api/listing",listingRouter);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next)=>{
     const statusCode = err.statusCode || 500;
