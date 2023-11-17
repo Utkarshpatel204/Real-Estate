4bd34662fcde379d07acd0d30815471f68d94665
Utkarsh <utkarshpatel204@gmail.com>
2023-11-17 11:44:10 +0530

    complete home and about page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
   const [searchTerm, setSearchTerm] = useState('');
   const navigate = useNavigate();
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const urlParams = new URLSearchParams(window.location.search);
@@ -28,7 +28,7 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
+            <span className='text-slate-500'>Utkarsh</span>
             <span className='text-slate-700'>Estate</span>
           </h1>
         </Link>
